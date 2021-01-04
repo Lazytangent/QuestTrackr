3703a49036731e8e5a16bbb744f51c25f584eb4c
Lazytangent <pm123633@gmail.com>
2021-01-04 17:07:34 -0600

    Added get /register

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { csrfProtection, asyncHandler } = require('./utils/utils');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
+  res.render('index', { title: 'QuestTrackr' });
 });
+
+router.get('/register', csrfProtection, asyncHandler(async (req, res) => {
+  res.render('register', { title: 'Register', csrfToken: req.csrfToken() });
+}));
 
 module.exports = router;
