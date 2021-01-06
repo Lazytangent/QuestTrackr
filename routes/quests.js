a92935fd534a0062916d1e046732dd90beb7d7fa
Mark Valdez <mhvaldez209@gmail.com>
2021-01-06 11:58:14 -0700

    router type error????

--- routes/quests.js
+++ routes/quests.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
-const bcrypt = require('bcryptjs');
 
 const db = require('../db/models');
 const { csrfProtection, asyncHandler } = require('./utils');
@@ -8,6 +7,42 @@ const { loginUser, logoutUser } = require('../authorization');// how will author
 
 const router = express.Router();
 
-router.get('/quest-create', asyncHandler, (req, res) => {
-    res.render('quest-create',)
+router.get('/quest-create', csrfProtection, (req, res) => {
+    const quest = db.Quest.build();
+    res.render('quest-create', {
+        title: 'Create a Quest',
+        quest,
+        csrfToken: req.csrfToken(),
+    });
 });
+
+// TODO taken from register post route, need to clean up...
+// router.post('/quest-create', csrfProtection, userValidators,
+//     asyncHandler(async (req, res, next) => {
+//         const {
+//             username,
+//             password,
+//             email 
+//         } = req.body;
+//         const user = db.User.build({
+//             username,
+//             email 
+//         });
+//         const validatorErrors = validationResult(req);
+
+//         if (validatorErrors.isEmpty()) {
+//             const hashedPassword = await bcrypt.hash(password, 10);
+//             user.hashedPassword = hashedPassword;
+//             await user.save();
+//             loginUser(req, res, user, next,);
+//         } else {
+//             const errors = validatorErrors.array().map((error) => error.msg);
+//             console.log(errors)
+//             res.render('register', {
+//                 title: 'Register',
+//                 user,
+//                 errors,
+//                 csrfToken: req.csrfToken(),
+//             });
+//         }
+//     }));
