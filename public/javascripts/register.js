5776cc541033a0d2cbc729ccad8c5634ed33d562
Lazytangent <pm123633@gmail.com>
2021-01-06 19:01:05 -0600

    Added a tags to link to home page from login and register pages

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,8 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
   const submitButton = document.querySelector('#submit');
-
-  const logoContainer = document.querySelector('.logo-container');
-  logoContainer.addEventListener('click', () => {
-    window.location.href = '/';
-  });
 });
