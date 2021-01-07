06e5958078b52260f7ede89243bd44e3d346f66c
Lazytangent <pm123633@gmail.com>
2021-01-06 18:04:58 -0600

    Cleared out comments

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const submitButton = document.querySelector('#submit');
 
-  // submitButton.setAttribute('disabled', true);
-  // Select required input fields
-
   const logoContainer = document.querySelector('.logo-container');
   logoContainer.addEventListener('click', () => {
     window.location.href = '/';
