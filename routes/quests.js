f7e4cb40494d043a9cfa26f131a6bd99c8bf3f97
Clint Hill <clint.hill07@gmail.com>
2021-01-07 13:23:02 -0500

    Start work on the users quests page.

--- routes/quests.js
+++ routes/quests.js
@@ -4,6 +4,19 @@ const { asyncHandler } = require('./utils');
 
 const router = express.Router();
 
+router.get('/', asyncHandler(async (req, res) => {
+  const username = 'Demo'
+  const testQuests = {
+    quest1: { title: 'Slay the dragon'},
+    quest2: { title: 'Collect the loot'},
+    quest3: { title: 'Save the princess if there is time'},
+  }
+  console.log('-------------------------')
+  const quests = await Quest.findAll();
+  console.log("Quests:", quests);
+  res.render('quest-list', { title: `${username}'s Active Quests`, testQuests });
+}));
+
 router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
   const questId = parseInt(req.params.id, 10);
   const quests = await Quest.findByPk(questId, { include: User })
