a2bcf621dc43323988a465aa309ac3f6ab47df05
Lazytangent <pm123633@gmail.com>
2021-01-08 11:14:01 -0600

    Removed the comments

--- db/seeders/20210108165452-seed-categories.js
+++ db/seeders/20210108165452-seed-categories.js
@@ -2,12 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-    */
     return queryInterface.bulkInsert('Categories', [
       {
         tag: 'Health',
@@ -91,12 +85,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-    */
       return queryInterface.bulkDelete('Categories', null, {});
   }
 };
