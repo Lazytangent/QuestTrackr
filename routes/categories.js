3765baeddd36d9a9be5735cb58e248f29e115a8c
Lazytangent <pm123633@gmail.com>
2021-03-27 15:23:51 -0500

    Forms and stuff

--- routes/categories.js
+++ routes/categories.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { asyncHandler } = require("./utils");
+
+const { asyncHandler, csrfProtection } = require("./utils");
 const { Category } = require("../db/models");
 
-router.get("/new", asyncHandler(async (req, res) => {
+router.get('', asyncHandler(async (_, res) => {
+  const categories = await Category.findAll();
+  return res.json({ categories });
+}));
+
+router.get("/new", csrfProtection, asyncHandler(async (req, res) => {
   const category = Category.build();
   return res.render("category-create", {
     title: "Create a Category",
     category,
+    csrfToken: req.csrfToken(),
   });
 }));
 
