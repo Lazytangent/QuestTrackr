2767edde3e047c3998713c97f81618a44176c52d
Lazytangent <pm123633@gmail.com>
2021-03-27 14:27:02 -0500

    Create Categories API router

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { asyncHandler } = require('./utils');
 const { UserQuest, Quest, User, Category, Sequelize } = require('../db/models');
+const categoriesRouter = require("./categories");
+
+router.use("/categories", categoriesRouter);
 
 router.put('/quests/:id(\\d+)', asyncHandler(async (req, res) => {
   const questId = parseInt(req.params.id, 10);
---
+++ routes/categories.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const { asyncHandler } = require("./utils");
+const { Category } = require("../db/models");
+
+module.exports = router;
