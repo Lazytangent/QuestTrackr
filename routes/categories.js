95512f2e40604feb7a9852e0302c85cde44ae9e1
Lazytangent <pm123633@gmail.com>
2021-03-27 14:30:38 -0500

    Get route for categories

--- routes/categories.js
+++ routes/categories.js
@@ -3,4 +3,9 @@ const router = express.Router();
 const { asyncHandler } = require("./utils");
 const { Category } = require("../db/models");
 
+router.get('', asyncHandler(async (_, res) => {
+  const categories = Category.findAll();
+  return res.json({ categories });
+}))
+
 module.exports = router;
